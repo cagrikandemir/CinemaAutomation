869f198804cee671210d3a03990b60906ef31973
cagrikandemir <cagrikandemir58@icloud.com>
2024-04-08 22:56:14 +0300

    Project
    Login Page Bitti

---
+++ CinemaAutomation/CinemaAutomation/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CinemaAutomation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txt_kullaniciadi_Click(object sender, EventArgs e)
+        {
+            txt_kullaniciadi.BackColor = Color.White;
+            panel3.BackColor = Color.White;
+            panel4.BackColor = SystemColors.Control;
+            txtsifre.BackColor=SystemColors.Control;
+        }
+
+        private void txtsifre_Click(object sender, EventArgs e)
+        {
+            txtsifre.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+            panel3.BackColor = SystemColors.Control;
+            txt_kullaniciadi.BackColor = SystemColors.Control;
+
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtsifre.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtsifre.UseSystemPasswordChar = true;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel3.BackColor = Color.White;
+            txt_kullaniciadi.BackColor = Color.White;
+        }
+    }
+}
